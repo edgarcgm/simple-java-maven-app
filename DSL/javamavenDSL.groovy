7c2a86cf8985f7e2c6a7bce3c7e193fae53ac7a3
Edgar Cortes Gallardo Medina <47435369+edgarcgm@users.noreply.github.com>
2025-06-14 11:28:10 -0600

    Update javamavenDSL.groovy

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/edgarcgm/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('edgarcgm')
+            node / gitConfigEmail('cortsedgar@gmail.com')
         }
     }
     steps {
@@ -31,8 +31,8 @@ job('Java Maven App DSL') {
             notifyBackToNormal(true)
             notifySuccess(true)
             notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
+            startNotification(true)
+            includeTestSummary(true)
             includeCustomMessage(false)
             customMessage(null)
             sendAs(null)
